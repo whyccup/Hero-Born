642056a59568ad38e64828483e4ae9549b28f9e3
haoyang.wu <haoyang.wu@thoughtworks.com>
2023-05-25 11:32:00 +0800

    fix: Fix the player falling bug, fix the wall-penetrating bug

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,13 +9,28 @@ public class PlayerBehavior : MonoBehaviour
 
     private float vInput;
     private float hInput;
+    private Rigidbody _rb;
 
-    void Update()
+	private void Start()
+	{
+        _rb = GetComponent<Rigidbody>();
+	}
+
+
+	private void Update()
     {
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        //this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        //this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
     }
+
+	private void FixedUpdate()
+	{
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + Time.fixedDeltaTime * vInput * this.transform.forward);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+	}
 }
